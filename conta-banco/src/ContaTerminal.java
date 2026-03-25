692b18a3eaf45b243d1b54e2110ba3c079e6c7e8
Rian <rianfavretto01@gmail.com>
2026-03-25 15:16:10 -0300

    feat: conclui desafio do terminal bancário com validação de dados e formatação

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int numeroConta;
+        String agencia, nomeCliente;
+        double saldo;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = scan.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = scan.next();
+
+        scan.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeCliente = scan.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        saldo = scan.nextDouble();
+
+        System.out.println("");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia.replaceFirst("(\\d{3})(\\d)", "$1-$2") + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque. ");
+
+        scan.close();
+    }
+}
